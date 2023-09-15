442d076db3149c135b1a761ed137810b847618cf
jmcavaddy <jakemcavaddy@gmail.com>
2023-09-15 15:20:04 -0400

    Updated manifest.json

--- client/src/Johnmcavaddy/components/Header/index.js
+++ client/src/Johnmcavaddy/components/Header/index.js
@@ -5,10 +5,10 @@ import resume from '../../../assets/John_McAvaddy_Resume.docx'
 const Header = ({ currentPage, handlePageChange }) => {
   return (
     <div className="container-fluid header">
-      <header className="d-flex justify-content-between align-items-center flex-column flex-md-row">
+      <header className="d-flex justify-content-between align-items-center flex-column flex-md-row text-center">
         <div>
           <h1 style={{ fontSize: '2.5rem' }}>
-            John McAvaddy
+            John (Jake) McAvaddy
           </h1>
           <h3><a href={resume} download>Download my resume!</a></h3>
         </div>
