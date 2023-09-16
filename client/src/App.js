c932cdceb54217b72cde618c62f2f8b8328683fe
jmcavaddy <jakemcavaddy@gmail.com>
2023-09-16 16:41:03 -0400

    Configuring apollo

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import NewEntry from "./Mernjournal/pages/NewEntry/index.js";
 import Footer from './Footer'
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
